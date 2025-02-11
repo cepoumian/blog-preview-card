727709e85854e6d3fa57c6bf591c368095844c1f
cepoumian <cepoumian@gmail.com>
2025-02-11 17:47:22 -0600

    Make pill compoent accept bg and text colors as atttributes

--- components/pill.js
+++ components/pill.js
@@ -1,8 +1,8 @@
 const template = document.createElement("template");
+
 template.innerHTML = /* html */ `
   <style>
     span {
-      background-color: var(--clr-yellow);
       padding: calc(var(--spacing-xs) / 2) var(--spacing-xs);
       border-radius: calc(var(--spacing-sm) / 4);
     }
@@ -13,10 +13,23 @@ template.innerHTML = /* html */ `
 `;
 
 class PillComponent extends HTMLElement {
+  static get observedAttributes() {
+    return ["bg-color", "text-color"];
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "bg-color") {
+      this.shadowRoot.querySelector("span").style.backgroundColor = newValue;
+    }
+    if (name === "text-color") {
+      this.shadowRoot.querySelector("span").style.color = newValue;
+    }
   }
 }
 
